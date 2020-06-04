430ce139dd36d2f731b8f5bae0f684ab31d82542
muzzik <45589707+1226085293@users.noreply.github.com>
2020-06-04 19:03:06 +0800

    Update critical.h
    # 删除不必要类
    # 修改乱码注释

--- other/raii/critical.h
+++ other/raii/critical.h
@@ -3,7 +3,7 @@
 #include "other/debug/dead_lock_check.h"
 
 namespace raii {
-	// ÁÙ½çÇø
+	// ä¸´ç•ŒåŒº
 	class critical {
 	private:
 		CRITICAL_SECTION* _crit;
@@ -14,7 +14,7 @@ namespace raii {
 		~critical();
 	};
 
-	// °²È«ÁÙ½çÇø
+	// å®‰å…¨ä¸´ç•ŒåŒº
 	class safe_critical {
 	private:
 		CRITICAL_SECTION* _cri;
@@ -24,12 +24,6 @@ namespace raii {
 		safe_critical(const critical&) = delete;
 		safe_critical& operator =(const critical&) = delete;
 		~safe_critical();
-	};
-
-	class safe_critical1 {
-	private:
-		CRITICAL_SECTION* _cri;
-		debug::dead_lock_check* _check;
 	};
 
 #include "source/critical.icc"
